f56e2f1137bc638f59685d52c024c3e6689a648c
razimbrao <rafaelozimbrao@gmail.com>
2022-08-23 17:07:21 -0300

    card de membros feito

--- src/components/layouts/CardMembers.jsx
+++ src/components/layouts/CardMembers.jsx
@@ -4,11 +4,24 @@ import '../css/CardMembers.css'
 
 const CardMembers = (props) => {
     return (
-        <div className="card-box">
-            <h3>{props.h3}</h3>
-            <p>
-                {props.text}
-            </p>
+        <div className="card-members-box">
+            <div className='card-members-title'>
+                <h2>{props.name}</h2>
+            </div>
+            <div className='card-members-content'>
+                <p>
+                    E-mail: <span>{props.email}</span>
+                </p>
+                <p>
+                    Aniversário: <span className='props-text'>{props.aniversario}</span>
+                </p>
+                <p>
+                    Departamentos: <span className='props-text'>{props.departamentos}</span>
+                </p>
+                <p>
+                    Cargo: <span className='props-text'>{props.cargo}</span>
+                </p>
+            </div>
         </div>
     )
 }
--- src/views/pages/Members.jsx
+++ src/views/pages/Members.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CardMembers from "../../components/layouts/CardMembers";
 import "./css/Members.css";
 
 const Members = () => {
@@ -6,8 +7,23 @@ const Members = () => {
         <section className="members-page">
             <section className="members-title">
                 <h2>Membros</h2>
+                <p>Conheça os membros da nossa Empresa Júnior!</p>
             </section>
             <section className="members-content">
+                <div className="members-list">
+                    <CardMembers name="Rafael de Oliveira Zimbrão" email="rafael.zimbrao@codejr.com.br" departamentos="marketing" aniversario="06/22" cargo="assessor"/>
+                    <CardMembers name="Rafael de Oliveira Zimbrão" email="rafael.zimbrao@codejr.com.br" departamentos="marketing" aniversario="06/22" cargo="assessor"/>
+                    <CardMembers name="Rafael de Oliveira Zimbrão" email="rafael.zimbrao@codejr.com.br" departamentos="marketing" aniversario="06/22" cargo="assessor"/>
+                    <CardMembers name="Rafael de Oliveira Zimbrão" email="rafael.zimbrao@codejr.com.br" departamentos="marketing" aniversario="06/22" cargo="assessor"/>
+                    <CardMembers name="Rafael de Oliveira Zimbrão" email="rafael.zimbrao@codejr.com.br" departamentos="marketing" aniversario="06/22" cargo="assessor"/>
+                    <CardMembers name="Rafael de Oliveira Zimbrão" email="rafael.zimbrao@codejr.com.br" departamentos="marketing" aniversario="06/22" cargo="assessor"/>
+                    <CardMembers name="Rafael de Oliveira Zimbrão" email="rafael.zimbrao@codejr.com.br" departamentos="marketing" aniversario="06/22" cargo="assessor"/>
+                    <CardMembers name="Rafael de Oliveira Zimbrão" email="rafael.zimbrao@codejr.com.br" departamentos="marketing" aniversario="06/22" cargo="assessor"/>
+                    <CardMembers name="Rafael de Oliveira Zimbrão" email="rafael.zimbrao@codejr.com.br" departamentos="marketing" aniversario="06/22" cargo="assessor"/>
+                    <CardMembers name="Rafael de Oliveira Zimbrão" email="rafael.zimbrao@codejr.com.br" departamentos="marketing" aniversario="06/22" cargo="assessor"/>
+                    <CardMembers name="Rafael de Oliveira Zimbrão" email="rafael.zimbrao@codejr.com.br" departamentos="marketing" aniversario="06/22" cargo="assessor"/>
+                    <CardMembers name="Rafael de Oliveira Zimbrão" email="rafael.zimbrao@codejr.com.br" departamentos="marketing" aniversario="06/22" cargo="assessor"/>
+                </div>
             </section>
         </section>
     )
