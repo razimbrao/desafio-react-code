91da8e46f6afc4ea3529f72af415577f9661dc63
razimbrao <rafaelozimbrao@gmail.com>
2022-08-26 23:51:37 -0300

    footer resp, TODO: botao navbar

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -6,7 +6,8 @@ const Navbar = () => {
     return (
         <header>
             <nav className="navbar">
-                <ul>
+                <ul className='default-nav'>
+
                     <div className='logo-nav'>
                         <Link to='/'>
                             <img src='/img/asta/asta-banner-nav.png' alt='logo da empresa' />
@@ -31,6 +32,26 @@ const Navbar = () => {
                             <Link to="/login">Login</Link>
                         </li>
                     </div>
+                </ul>
+                <ul className='dropdown-nav'>
+                    <span class="material-symbols-outlined">
+                        menu
+                    </span>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">Sobre</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contato</Link>
+                    </li>
+                    <li>
+                        <Link to="/members">Membros</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
                 </ul>
             </nav>
         </header>
